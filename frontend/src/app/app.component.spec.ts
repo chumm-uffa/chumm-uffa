291d133245267099ed2191f2c2ee83277105e23f
heeeman <ckalt@hsr.ch>
2017-11-20 21:45:18 +0100

    unit tests in Betrieb genommen -> ein noch etwas ungünstiges Example

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,27 +1,46 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {AppRoutingModule} from "./app-routing.module";
+import {LoginModule} from "./login/login.modul";
+import {RegistrationModule} from "./registration/registration.module";
+import {APP_BASE_HREF} from "@angular/common";
+
+
+/**
+ * Demo Unit Test
+ * Sollte aber wieder raus. Da er die Root Componente Testet wird er immer wieder Probleme machen
+ * und viel zu fett werden.
+ */
+
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [AppRoutingModule, LoginModule, RegistrationModule],
       declarations: [
-        AppComponent
+        AppComponent, HeaderComponent
       ],
+      providers: [{provide: APP_BASE_HREF, useValue : '/' }]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Come-up Header');
   }));
 });
