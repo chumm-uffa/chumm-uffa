7d3301c25f31def256626d00acdf9e0f55118156
heeeman <ckalt@hsr.ch>
2017-11-26 09:31:29 +0100

    #8 Bsp Reactiv Form inkl. Validierung 1. Wurf

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,8 +1,39 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {validatePwdsMatch} from '../shared/validators/password-match.validator';
 
 @Component({
-  selector: 'registration',
+  selector: 'app-registration',
   templateUrl: './registration.component.html'
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit{
+
+
+  loginForm: FormGroup;
+
+  constructor( private fB: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fB.group({
+      username: ['name', [Validators.required, Validators.minLength(2)]], // Field , Fieldvalidators
+      password: ['pwd', [Validators.required, Validators.minLength(8)]],
+      password2: ['pwd2', [Validators.required, Validators.minLength(8)]],
+      sex: 'm',
+      email: ['myMail@dot.ch', [Validators.email]]
+    }, {
+      validator: validatePwdsMatch('password', 'password2')  // Formvalidators -> validate between Fields
+    });
+  }
+
+  onClickRegister() {
+    if (this.loginForm.valid && !this.loginForm.pending) {  // Form ist gültig und die Validierung ist abgeschlossen
+      console.log('form value', this.loginForm.value);
+      console.log('send data to Service');
+    }else {
+      console.log('form invald', this.loginForm.errors);
+      console.log('form invald', this.loginForm.hasError('passwordMismatch'));
+    }
+  }
+
+
 }
--- frontend/src/app/registration/registration.module.ts
+++ frontend/src/app/registration/registration.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
-import {RegistrationComponent} from "./registration.component";
+import {RegistrationComponent} from './registration.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 
 
 @NgModule({
+  imports: [
+    ReactiveFormsModule, CommonModule
+  ],
   declarations: [
     RegistrationComponent
   ]
