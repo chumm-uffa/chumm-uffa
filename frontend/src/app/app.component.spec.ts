6d8d9aaf86aab774db3775238c17c669a39aea09
heeeman <ckalt@hsr.ch>
2017-12-07 19:03:43 +0100

    #23 runTestrun -> es grünt so grün

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,19 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from './app-routing.module';
 import {LoginModule} from './login/login.modul';
 import {RegistrationModule} from './registration/registration.module';
 import {APP_BASE_HREF} from '@angular/common';
 import {HomeComponent} from './home/home.component';
+import {MymeetupsComponent} from './mymeetups/mymeetups.component';
+import {MeetupComponent} from './meetup/meetup.component';
+import {OwnMeetupsComponent} from './mymeetups/own-meetups/own-meetups.component';
+import {OpenRequestsComponent} from './mymeetups/open-requests/open-requests.component';
+import {SharedModule} from './shared/shared.module';
+import {TranslateModule} from '@ngx-translate/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {AppStateService} from './core/app-state.service';
 
 
 /**
@@ -18,11 +26,18 @@ import {HomeComponent} from './home/home.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [AppRoutingModule, LoginModule, RegistrationModule],
+      imports: [AppRoutingModule, LoginModule, RegistrationModule, SharedModule,
+        TranslateModule.forRoot(), ReactiveFormsModule],
       declarations: [
-        AppComponent, HeaderComponent, HomeComponent
+        AppComponent,
+        HeaderComponent,
+        HomeComponent,
+        MymeetupsComponent,
+        OwnMeetupsComponent,
+        OpenRequestsComponent,
+        MeetupComponent
       ],
-      providers: [{provide: APP_BASE_HREF, useValue : '/' }]
+      providers: [AppStateService, {provide: APP_BASE_HREF, useValue: '/'}]
     }).compileComponents();
   }));
 
